8850a0664c0232e9391da71a0dffb3ff005d0ea1
Kevin-TD <kdiggs0618@gmail.com>
2023-12-13 13:38:47 -0500

    ci runs python file to check formatting

---
+++ check_formatting.py
@@ -0,0 +1,15 @@
+#! python3 check_formatting.py
+# must be in ./c-resource-leak-checker
+
+import os
+import sys
+
+# checks if cwd is /c-resource-leak-checker
+if os.path.split(os.getcwd())[1] != "c-resource-leak-checker":
+    print(f"WARNING: not in dir /c-resource-leak-checker; cwd is {os.getcwd()}")
+    sys.exit(1)
+
+if (os.system("! astyle --dry-run --style=google --exclude=build --recursive *.cpp,*.h,*.c -n | grep Formatted") != 0):
+    sys.exit(1)
+else:
+    sys.exit(0)
