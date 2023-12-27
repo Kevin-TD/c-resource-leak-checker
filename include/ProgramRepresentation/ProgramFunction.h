e7905dd1d2e122642e6653b80d2821dec4a7a82b
Kevin-TD <kdiggs0618@gmail.com>
2023-12-27 09:53:45 -0500

    Bug fixes
    Bug 1: A struct is referenced in a different branch than the one it was defined in for certain gepInst instructions. This caused segfaults in the alias reasoning. Fixed by creating a function to search an entire program function for a certain value pointer or instruction.
    
    Bug 2: addAlias method of DisjointedPVAliasSets did not handle case where both sets exist. This case is now explicitly handled. addAlias code also cleaner.
    
    Additional test cases added

--- include/ProgramRepresentation/ProgramFunction.h
+++ include/ProgramRepresentation/ProgramFunction.h
@@ -38,38 +38,38 @@ class ProgramFunction {
     ProgramPoint *getProgramPointRef(const std::string &pointName,
                                      bool addNewIfNotFound);
 
-    // searches every program point to find the alias set with that value pointer. this
-    // method should only be used if you need access to an alias set that does not
-    // exist in your current branch, but in a preceding one.
-    /*
-    Example situation:
+  // searches every program point to find the alias set with that value pointer. this 
+  // method should only be used if you need access to an alias set that does not 
+  // exist in your current branch, but in a preceding one.
+  /*
+  Example situation: 
 
-    struct M {
-      char* x;
-      char* y;
-    };
+  struct M {
+    char* x;
+    char* y;
+  };
 
-    int main() {
-      struct M m;
-      m.x = (char*)malloc(15);
-      m.y = m.x;
-      [[ m.x and m.y are aliased ]]
+  int main() {
+    struct M m; 
+    m.x = (char*)malloc(15);
+    m.y = m.x;
+    [[ m.x and m.y are aliased ]]
 
-      if (getchar()) {
-        free(m.y);
-        [[ in the IR, we detect that we are
-        referencing field y of struct M via a gepInst (effectively breaks down
-        a struct into its fields, see docs: https://llvm.org/docs/GetElementPtr.html)
-
-        we need a ref (Value*) to that struct to build this new program variable m.y and add
-        necessary aliases. that ref is in the entry/previous program point, however. thus,
-        to access it, we call this method. ]]
-      }
+    if (getchar()) {
+      free(m.y);
+      [[ in the IR, we detect that we are 
+      referencing field y of struct M via a gepInst (effectively breaks down
+      a struct into its fields, see docs: https://llvm.org/docs/GetElementPtr.html)
+      
+      we need a ref (Value*) to that struct to build this new program variable m.y and add
+      necessary aliases. that ref is in the entry/previous program point, however. thus, 
+      to access it, we call this method. ]]
     }
-    */
-    PVAliasSet *getPVASRefFromValue(Value* value);
-
-    std::string getFunctionName() const;
+  }
+  */
+  PVAliasSet *getPVASRefFromValue(Value* value); 
+  
+  std::string getFunctionName() const;
 };
 
 #endif
