37dcc25946a552ac3c67f72efb7f0c117ffb263e
Kevin-TD <kdiggs0618@gmail.com>
2024-01-25 16:25:56 -0500

    merging fixes

--- src/TestRunner.cpp
+++ src/TestRunner.cpp
@@ -23,51 +23,6 @@ bool TestRunner::runTests(const std::string functionName,
 
         logout("branch = " << branchName);
 
-        DisjointedPVAliasSets expectedDPVAS =
-            expectedResultPoint.getProgramVariableAliasSets();
-
-        ProgramPoint receivedResultPoint =
-            receivedResult.getProgramPoint(branchName, true);
-
-        for (PVAliasSet expctedPVAS : expectedDPVAS.getSets()) {
-            std::set<std::string> expectedSet =
-                expctedPVAS.getMethodsSet().getMethods();
-            std::string expectedSetString = rlc_util::setToString(expectedSet);
-
-            // TODO: change structure for expectedResult.
-            // expctedPVAS.getProgramVariables().size() will always equal 1, so
-            // this for loop is misleading
-            for (ProgramVariable expectedPV : expctedPVAS.getProgramVariables()) {
-                std::string expectedPVName = expectedPV.getCleanedName();
-                std::string expectedPVRefName = expectedPVName;
-
-                if (structFieldToIndexMap.structNameAndFieldIsInMap(expectedPVName)) {
-                    expectedPVRefName = structFieldToIndexMap.get(expectedPVName);
-                }
-
-                PVAliasSet *receivedPVAS =
-                    receivedResultPoint.getPVASRef(expectedPVRefName, true);
-
-                std::set<std::string> receivedSet =
-                    receivedPVAS->getMethodsSet().getMethods();
-                std::string receivedSetString = rlc_util::setToString(receivedSet);
-
-                errs() << "Test for branch name = " << branchName
-                       << " var name = " << expectedPVName;
-
-                if (expectedSet == receivedSet) {
-                    errs() << " passed\n";
-                } else {
-                    errs() << " **FAILED**\n";
-                    testPassed = EXIT_FAILURE;
-                }
-                errs() << "EXPECTED " << expectedSetString << "\n";
-                errs() << "RECEIVED " << receivedSetString << "\n\n";
-            }
-        }
-
-        logout("branch = " << branchName);
-
         DisjointPVAliasSets expectedDPVAS =
             expectedResultPoint.getProgramVariableAliasSets();
 
