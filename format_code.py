6a5317ed653948872da3c324acb5b0f29b45781d
Kevin-TD <kdiggs0618@gmail.com>
2024-01-28 16:48:53 -0500

    creates python formatting
    formatting also integrated into ci

--- format_code.py
+++ format_code.py
@@ -6,7 +6,9 @@ import sys
 
 # checks if cwd is /c-resource-leak-checker
 if os.path.split(os.getcwd())[1] != "c-resource-leak-checker":
-    print(f"WARNING: not in dir /c-resource-leak-checker; cwd is {os.getcwd()}")
+    print(
+        f"WARNING: not in dir /c-resource-leak-checker; cwd is {os.getcwd()}")
     sys.exit(1)
 
 os.system("astyle --style=google --exclude=build --ignore-exclude-errors --recursive *.cpp,*.h,*.c -n")
+os.system("autopep8 --in-place --recursive --ignore=E402 .")
