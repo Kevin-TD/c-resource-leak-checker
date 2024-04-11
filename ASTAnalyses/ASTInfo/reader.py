2463598d28e80e2a843f46465f8fdc9104d98c21
Kevin-TD <kdiggs0618@gmail.com>
2024-04-10 23:59:42 -0400

    progress save

--- ASTAnalyses/ASTInfo/reader.py
+++ ASTAnalyses/ASTInfo/reader.py
@@ -24,41 +24,111 @@ def collect_until(text_io_wrapper, char):
     
     return res
 
-
     
+'''
+syntax description 
 
-with open(file_to_read) as ast_info:
-    '''
-    syntax description 
+There are 4 types of declaration: FUNCTION, STRUCT, STRUCT_VARIABLE, and ANNOTATION. An asterik is put before the name to indicate that it's a declaration; e.g., *FUNCTION. 
 
-    There are 4 types of declaration: FUNCTION, STRUCT, STRUCT_VARIABLE, and ANNOTATION. An asterik is put before the name to indicate that it's a declaration; e.g., *FUNCTION. 
+A function declaration *FUNCTION must be followwed by @NAME(string), @RETURN_TYPE(string), and @PARAMETERS[list of strings]
 
-    A function declaration *FUNCTION must be followwed by @NAME(string), @RETURN_TYPE(string), and @PARAMETERS[list of strings]
+A struct declaration *STRUCT must be followed by @NAME(string)
 
-    A struct declaration *STRUCT must be followed by @NAME(string)
+For *STRUCT_VARIABLE, it has @NAME(string), @TYPE(string)
 
-    For *STRUCT_VARIABLE, it has @NAME(string), @TYPE(string)
+For *ANNOTATION, it has @STRING(string)
+''' 
 
-    For *ANNOTATION, it has @STRING(string)
-    
-    
-    ''' 
-    char = ast_info.read(1)
+
+ast_info = open(file_to_read, "r")
+
+char = ast_info.read(1)
+
+while char:
 
     if char == "*":
         declaration_type = collect_until(ast_info, "\n")
-        print(f"'{declaration_type}'")
+        logout(f"'{declaration_type}'")
 
         if declaration_type == "FUNCTION":  
-            attribute_type = collect_until(ast_info, " ")
-            print(f"'{attribute_type}'")
+            name_decl = collect_until(ast_info, " ")
+            logout(f"'{name_decl}'")
 
-            if attribute_type == "@NAME":
+            if name_decl == "@NAME":
                 name_data = collect_until(ast_info, ")")[1:]
-                print(f"'{name_data}'")
-            elif attribute_type == "@RETURN_TYPE":
+                logout(f"'{name_data}'")
+            else: 
+                raise SyntaxError("Expected @NAME decl after *FUNCTION")
+            
+            return_type_decl = collect_until(ast_info, " ")[1:]
+            logout(f"'{return_type_decl}'")
+
+            if return_type_decl == "@RETURN_TYPE":
                 return_type_data = collect_until(ast_info, ")")[1:]
-                print(f"'{return_type_data}'")
-            elif attribute_type == "@PARAMETERS":
-                paramters_data = collect_until(ast_info, "]")[1:]
-                print(f"'{paramters_data}'")
+                logout(f"'{return_type_data}'")
+            
+            created_function = Function(name_data, return_type_data)
+            
+            # TODO: get changes from draft pull request into this one. manual copy paste
+            # TODO: int main function takes no args. make it so when arg list is empty it's just PARAMETERS []. same with STRUCT @FIELDS
+            # TODO: more errors
+
+            parameters_decl = collect_until(ast_info, " ")[1:]
+            logout(f"'{parameters_decl}'")
+
+            if parameters_decl == "@PARAMETERS":
+                parameters_data = collect_until(ast_info, "]")[1:].split(",")
+                logout(f"'{parameters_data}'")
+                for (i, param) in enumerate(parameters_data):
+                    if param == "":
+                        continue
+
+                    created_function.add_parameter(Parameter(i, param))
+            
+        
+
+        elif declaration_type == "STRUCT":
+            name_decl = collect_until(ast_info, " ")
+            logout(f"'{name_decl}'")
+
+            if name_decl == "@NAME":
+                name_data = collect_until(ast_info, ")")[1:]
+                logout(f"'{name_data}'")
+            
+            fields_decl = collect_until(ast_info, " ")[1:]
+            logout(f"'{fields_decl}'")
+
+            if fields_decl == "@FIELDS":
+                fields_data = collect_until(ast_info, "]")[1:].split(",")
+                logout(f"'{fields_data}'")
+        
+        elif declaration_type == "STRUCT_VARIABLE":
+            name_decl = collect_until(ast_info, " ")
+            logout(f"'{name_decl}'")
+
+            if name_decl == "@NAME":
+                name_data = collect_until(ast_info, ")")[1:]
+                logout(f"'{name_data}'")
+            
+            type_decl = collect_until(ast_info, " ")[1:]
+            logout(f"'{type_decl}'")
+
+            if type_decl == "@TYPE":
+                type_data = collect_until(ast_info, ")")[1:]
+                logout(f"'{type_data}'")
+        
+        elif declaration_type == "ANNOTATION":
+            string_decl = collect_until(ast_info, " ")
+            logout(f"'{string_decl}'")
+
+            if string_decl == "@STRING":
+                string_data = collect_until(ast_info, "\n")[1:]
+                string_data = string_data[:len(string_data) - 1]
+                logout(f"'{string_data}'")
+            
+        
+        logout("")
+    
+    char = ast_info.read(1)
+
+ast_info.close()
