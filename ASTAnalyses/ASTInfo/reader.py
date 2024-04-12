45d7a641f64fb2c4a62cc24289a98dfccef31784
Kevin-TD <kdiggs0618@gmail.com>
2024-04-11 20:51:49 -0400

    formalized into a class

--- ASTAnalyses/ASTInfo/reader.py
+++ ASTAnalyses/ASTInfo/reader.py
@@ -11,41 +11,6 @@ from ASTAnalyses.ASTInfo.Debug import *
 # (build dir)
 # python3 ../ASTAnalyses/ASTInfo/reader.py testASTOutput.txt
 
-file_to_read = sys.argv[1]
-
-
-def collect_until(text_io_wrapper, char):
-    res = ""
-    read_char = text_io_wrapper.read(1)
-
-    while (read_char != char):
-        res += read_char
-        read_char = text_io_wrapper.read(1)
-
-    return res
-
-
-def collect_and_validate(text_io_wrapper, char, equals_to):
-    res = ""
-    read_char = text_io_wrapper.read(1)
-
-    while (read_char != char):
-        res += read_char
-        read_char = text_io_wrapper.read(1)
-
-    if res != equals_to:
-        raise SyntaxError(f"Expected {equals_to} but received {res}")
-
-    return res
-
-
-def skip(text_io_wrapper, char):
-    res = text_io_wrapper.read(1)
-
-    if res != char:
-        raise ValueError(f"{char} not skipped but {res}")
-
-
 '''
 syntax description 
 
@@ -61,102 +26,165 @@ For *ANNOTATION, it has @ANNO_TYPE(string), @TARGET<string>, and @METHODS [list 
 '''
 
 
-ast_info = open(file_to_read, "r")
+class ASTReader:
+    """
+    Reads AST information in and gets the functions, structs, struct vars, and annotations into easier-to-handle
+    classes (Function, Struct, StructVar, and Annotation)
+    """
 
-char = ast_info.read(1)
+    def __init__(self, file_to_read):
+        self.__file_stream = open(file_to_read, "r")
+        self.__functions: list[Function] = []
+        self.__structs: list[Struct] = []
+        self.__struct_vars: list[StructVar] = []
+        self.__annotations: list[Annotation] = []
 
-while char:
+        char = self.__file_stream.read(1)
+        while char:
+            if char == "*":
+                declaration_type = self.__collect_until("\n")
+                if declaration_type == "FUNCTION":
 
-    if char == "*":
-        declaration_type = collect_until(ast_info, "\n")
+                    self.__collect_and_validate(" ", "@NAME")
+                    self.__skip("(")
+                    name_data = self.__collect_until(")")
+                    self.__skip("\n")
 
-        if declaration_type == "FUNCTION":
+                    self.__collect_and_validate(" ", "@RETURN_TYPE")
+                    self.__skip("(")
+                    return_type_data = self.__collect_until(")")
+                    self.__skip("\n")
 
-            name_decl = collect_and_validate(ast_info, " ", "@NAME")
-            skip(ast_info, "(")
-            name_data = collect_until(ast_info, ")")
-            skip(ast_info, "\n")
+                    created_function = Function(name_data, return_type_data)
 
-            return_type_decl = collect_and_validate(
-                ast_info, " ", "@RETURN_TYPE")
-            skip(ast_info, "(")
-            return_type_data = collect_until(ast_info, ")")
-            skip(ast_info, "\n")
+                    self.__collect_and_validate(" ", "@PARAMETERS")
+                    self.__skip("[")
+                    parameters_data = self.__collect_until("]").split(",")
 
-            created_function = Function(name_data, return_type_data)
+                    for (i, param) in enumerate(parameters_data):
+                        if param == "":
+                            continue
 
-            parameters_decl = collect_and_validate(
-                ast_info, " ", "@PARAMETERS")
-            skip(ast_info, "[")
-            parameters_data = collect_until(ast_info, "]").split(",")
+                        logout(
+                            f"FUNC: {name_data}, {return_type_data}, {i}, {param}")
+                        created_function.add_parameter(Parameter(i, param))
 
-            for (i, param) in enumerate(parameters_data):
-                if param == "":
-                    continue
+                    self.__functions.append(created_function)
 
-                logout(
-                    f"FUNC: {name_data}, {return_type_data}, {i}, {param}")
-                created_function.add_parameter(Parameter(i, param))
+                elif declaration_type == "STRUCT":
+                    self.__collect_and_validate(" ", "@NAME")
+                    self.__skip("(")
+                    name_data = self.__collect_until(")")
+                    self.__skip("\n")
 
-        elif declaration_type == "STRUCT":
-            name_decl = collect_and_validate(ast_info, " ", "@NAME")
-            skip(ast_info, "(")
-            name_data = collect_until(ast_info, ")")
-            skip(ast_info, "\n")
+                    created_struct = Struct(name_data)
 
-            created_struct = Struct(name_data)
+                    self.__collect_and_validate(" ", "@FIELDS")
+                    self.__skip("[")
+                    fields_data = self.__collect_until("]").split(",")
 
-            fields_decl = collect_and_validate(ast_info, " ", "@FIELDS")
-            skip(ast_info, "[")
-            fields_data = collect_until(ast_info, "]").split(",")
+                    for (i, field) in enumerate(fields_data):
+                        if field == "":
+                            continue
 
-            for (i, field) in enumerate(fields_data):
-                if field == "":
-                    continue
+                        logout(
+                            f"STRUCT: {name_data}, {return_type_data}, {i}, {field}")
+                        created_struct.add_field(Field(field, i))
 
-                logout(
-                    f"STRUCT: {name_data}, {return_type_data}, {i}, {field}")
-                created_struct.add_field(Field(field, i))
+                    self.__structs.append(created_struct)
 
-        elif declaration_type == "STRUCT_VARIABLE":
-            name_decl = collect_and_validate(ast_info, " ", "@NAME")
-            skip(ast_info, "(")
-            name_data = collect_until(ast_info, ")")
-            skip(ast_info, "\n")
+                elif declaration_type == "STRUCT_VARIABLE":
+                    self.__collect_and_validate(" ", "@NAME")
+                    self.__skip("(")
+                    name_data = self.__collect_until(")")
+                    self.__skip("\n")
 
-            type_decl = collect_and_validate(ast_info, " ", "@TYPE")
-            skip(ast_info, "(")
-            type_data = collect_until(ast_info, ")")
+                    self.__collect_and_validate(" ", "@TYPE")
+                    self.__skip("(")
+                    type_data = self.__collect_until(")")
 
-            created_struct_var = StructVar(name_data, type_data)
-            logout(f"STRUCT_VAR: {name_data}, {type_data}")
+                    created_struct_var = StructVar(name_data, type_data)
+                    logout(f"STRUCT_VAR: {name_data}, {type_data}")
 
-        elif declaration_type == "ANNOTATION":
-            anno_type_decl = collect_and_validate(ast_info, " ", "@ANNO_TYPE")
-            skip(ast_info, "(")
-            anno_type_data = collect_until(ast_info, ")")
-            skip(ast_info, "\n")
+                    self.__struct_vars.append(created_struct_var)
 
-            target_decl = collect_and_validate(ast_info, " ", "@TARGET")
-            skip(ast_info, "<")
-            target_data = collect_until(ast_info, ">")
-            skip(ast_info, "\n")
+                elif declaration_type == "ANNOTATION":
+                    self.__collect_and_validate(" ", "@ANNO_TYPE")
+                    self.__skip("(")
+                    anno_type_data = self.__collect_until(")")
+                    self.__skip("\n")
 
-            methods_decl = collect_and_validate(ast_info, " ", "@METHODS")
-            skip(ast_info, "[")
-            methods_data = collect_until(ast_info, "]").split(",")
-            methods_str = ""
+                    self.__collect_and_validate(" ", "@TARGET")
+                    self.__skip("<")
+                    target_data = self.__collect_until(">")
+                    self.__skip("\n")
 
-            for (i, method) in enumerate(methods_data):
-                methods_str += method
-                if (i != len(methods_data) - 1):
-                    methods_str += ","
+                    self.__collect_and_validate(" ", "@METHODS")
+                    self.__skip("[")
+                    methods_data = self.__collect_until("]").split(",")
+                    methods_str = ""
 
-            created_annotation = Annotation(
-                anno_type_data, target_data, methods_str)
-            logout(
-                f"ANNOTATION: {anno_type_data}, {target_data}, {methods_str}")
+                    for (i, method) in enumerate(methods_data):
+                        methods_str += method
+                        if (i != len(methods_data) - 1):
+                            methods_str += ","
 
-    char = ast_info.read(1)
+                    created_annotation = Annotation(
+                        anno_type_data, target_data, methods_str)
+                    logout(
+                        f"ANNOTATION: {anno_type_data}, {target_data}, {methods_str}")
 
-ast_info.close()
+                    self.__annotations.append(created_annotation)
+
+            char = self.__file_stream.read(1)
+
+        self.__file_stream.close()
+
+    def __skip(self, char):
+        """
+        Reads current character and errors if what is read is not equal to char
+        """
+        res = self.__file_stream.read(1)
+
+        if res != char:
+            raise ValueError(f"{char} not skipped but {res}")
+
+    def __collect_until(self, char):
+        """
+        Reads file stream until char is hit 
+        """
+
+        res = ""
+        read_char = self.__file_stream.read(1)
+
+        while (read_char != char):
+            res += read_char
+            read_char = self.__file_stream.read(1)
+
+        return res
+
+    def __collect_and_validate(self, char, equals_to):
+        """
+        Reads file stream until char is hit. Throws and error if what was read does not equal equals_to.
+        """
+        res = ""
+        read_char = self.__file_stream.read(1)
+
+        while (read_char != char):
+            res += read_char
+            read_char = self.__file_stream.read(1)
+
+        if res != equals_to:
+            raise SyntaxError(f"Expected {equals_to} but received {res}")
+
+    def get_functions(self):
+        return self.__functions
+
+    def get_structs(self):
+        return self.__structs
+
+    def get_struct_vars(self):
+        return self.__struct_vars
+
+    def get_annotations(self):
+        return self.__annotations
