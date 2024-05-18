6c899aa4648faa9c2574e024d9150a9cbd4f1a11
Kevin-TD <kdiggs0618@gmail.com>
2024-05-18 06:51:50 -0400

    todo added

--- src/Analysis.cpp
+++ src/Analysis.cpp
@@ -29,6 +29,7 @@
 // TODO:  CLT that generates AST based on test name (for debugging). if file name given, pipe into file. if not, log to terminal
 // TODO: see for ast info generator we can filter out functions included from std lib
 // TODO: make test cases for ASTAnalyses and have them ignore functions in stdlib
+// TODO: add testing to ast_info_tokens/generator/reader to ensure that tweaking format doesn't change pass results
 
 // TODO!: includes_test fails because IR does desugaring sometimes and we currently cannot reverse
 // it. to be fixed with AST pass
