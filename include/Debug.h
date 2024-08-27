241c656a99959ce94b8ae087eef700158b2f6f2d
Kevin-TD <kdiggs0618@gmail.com>
2024-08-27 09:36:52 -0400

    Update Debug.h

--- include/Debug.h
+++ include/Debug.h
@@ -3,7 +3,7 @@
 #define DEBUG true
 #define UTIL_VARIABLE_FUNC_DEBUG false
 #define BUILD_PROGRAM_LINES_BRANCH_INFO false
-#define RUN_UTIL_FUNCTION_TESTS false
+#define RUN_UTIL_FUNCTION_TESTS true
 
 #if DEBUG
 #define logout(x) errs() << x << "\n"
