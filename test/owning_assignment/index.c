f9d75884d1ae9db5aff8b0a7f63f4b7dfb8f56aa
Robert Blacha <rb97@njit.edu>
2025-12-04 14:48:28 -0500

    tests added

--- test/owning_assignment/index.c
+++ test/owning_assignment/index.c
@@ -4,19 +4,19 @@
 #include "../../Annotations/Annotations.h"
 
 typedef struct Box {
-	long *inside;
+    long *inside;
 } Box;
 
 int MustCall("free", "inside") foo(Box *b) {
-	long *item = malloc(sizeof(long));
-	*item = 1;
-	b->inside = item;
+    long *item = malloc(sizeof(long));
+    *item = 1;
+    b->inside = item;
 }
 
 int main () {
-	Box *box = malloc(sizeof(Box));
-	foo(box);
-	free(box->inside);
-	free(box);
+    Box *box = malloc(sizeof(Box));
+    foo(box);
+    free(box->inside);
+    free(box);
     return 0;
 }
